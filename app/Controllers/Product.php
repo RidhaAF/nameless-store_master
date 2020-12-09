c73d454f4226e396fa371327d40756610e06aa7a
Ridha Ahmad Firdaus <ridhaaf@gmail.com>
2020-12-09 20:50:50 +0700

    Update Product

---
+++ app/Controllers/Product.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\ProductModel;
+
+class Product extends BaseController
+{
+    protected $productModel;
+
+    public function __construct()
+    {
+        $this->productModel = new ProductModel;
+    }
+}
