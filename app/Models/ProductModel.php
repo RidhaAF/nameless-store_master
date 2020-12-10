df3962eb631331b909cccfd82762fc9f0f1611dd
Ridha Ahmad Firdaus <ridhaaf@gmail.com>
2020-12-10 22:56:03 +0700

    Update ProductModel.php

--- app/Models/ProductModel.php
+++ app/Models/ProductModel.php
@@ -4,7 +4,13 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class ProductModel extends Product
+class ProductModel extends Model
 {
     protected $table = 'product';
+    protected $allowedFields = ['brand', 'type'];
+
+    public function search($keyword)
+    {
+        return $this->table('product')->like('brand', $keyword)->orLike('type', $keyword);
+    }
 }
